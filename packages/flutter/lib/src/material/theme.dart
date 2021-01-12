970c865fddff79dc458b99b88974f2aba1a6b7c6
mengsieng <mengsieng.him@trendsoftinnovation.com>
2021-01-12 17:30:39 +0700

    add photo

--- packages/flutter/lib/src/material/theme.dart
+++ packages/flutter/lib/src/material/theme.dart
@@ -41,9 +41,9 @@ class Theme extends StatelessWidget {
     Key? key,
     required this.data,
     required this.child,
-  }) : assert(child != null),
-       assert(data != null),
-       super(key: key);
+  })   : assert(child != null),
+        assert(data != null),
+        super(key: key);
 
   /// Specifies the color and typography values for descendant widgets.
   final ThemeData data;
@@ -105,11 +105,15 @@ class Theme extends StatelessWidget {
   /// }
   /// ```
   static ThemeData of(BuildContext context) {
-    final _InheritedTheme? inheritedTheme = context.dependOnInheritedWidgetOfExactType<_InheritedTheme>();
-    final MaterialLocalizations? localizations = Localizations.of<MaterialLocalizations>(context, MaterialLocalizations);
-    final ScriptCategory category = localizations?.scriptCategory ?? ScriptCategory.englishLike;
+    final _InheritedTheme? inheritedTheme =
+        context.dependOnInheritedWidgetOfExactType<_InheritedTheme>();
+    final MaterialLocalizations? localizations =
+        Localizations.of<MaterialLocalizations>(context, MaterialLocalizations);
+    final ScriptCategory category =
+        localizations?.scriptCategory ?? ScriptCategory.englishLike;
     final ThemeData theme = inheritedTheme?.theme.data ?? _kFallbackTheme;
-    return ThemeData.localize(theme, theme.typography.geometryThemeFor(category));
+    return ThemeData.localize(
+        theme, theme.typography.geometryThemeFor(category));
   }
 
   @override
@@ -134,7 +138,8 @@ class Theme extends StatelessWidget {
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
-    properties.add(DiagnosticsProperty<ThemeData>('data', data, showName: false));
+    properties
+        .add(DiagnosticsProperty<ThemeData>('data', data, showName: false));
   }
 }
 
@@ -143,8 +148,8 @@ class _InheritedTheme extends InheritedTheme {
     Key? key,
     required this.theme,
     required Widget child,
-  }) : assert(theme != null),
-       super(key: key, child: child);
+  })   : assert(theme != null),
+        super(key: key, child: child);
 
   final Theme theme;
 
@@ -169,7 +174,8 @@ class ThemeDataTween extends Tween<ThemeData> {
   /// The [begin] and [end] properties must be non-null before the tween is
   /// first used, but the arguments can be null if the values are going to be
   /// filled in later.
-  ThemeDataTween({ ThemeData? begin, ThemeData? end }) : super(begin: begin, end: end);
+  ThemeDataTween({ThemeData? begin, ThemeData? end})
+      : super(begin: begin, end: end);
 
   @override
   ThemeData lerp(double t) => ThemeData.lerp(begin!, end!, t);
@@ -201,9 +207,9 @@ class AnimatedTheme extends ImplicitlyAnimatedWidget {
     Duration duration = kThemeAnimationDuration,
     VoidCallback? onEnd,
     required this.child,
-  }) : assert(child != null),
-       assert(data != null),
-       super(key: key, curve: curve, duration: duration, onEnd: onEnd);
+  })   : assert(child != null),
+        assert(data != null),
+        super(key: key, curve: curve, duration: duration, onEnd: onEnd);
 
   /// Specifies the color and typography values for descendant widgets.
   final ThemeData data;
@@ -223,7 +229,9 @@ class _AnimatedThemeState extends AnimatedWidgetBaseState<AnimatedTheme> {
   @override
   void forEachTween(TweenVisitor<dynamic> visitor) {
     // TODO(ianh): Use constructor tear-offs when it becomes possible, https://github.com/dart-lang/sdk/issues/10659
-    _data = visitor(_data, widget.data, (dynamic value) => ThemeDataTween(begin: value as ThemeData))! as ThemeDataTween;
+    _data = visitor(_data, widget.data,
+            (dynamic value) => ThemeDataTween(begin: value as ThemeData))!
+        as ThemeDataTween;
   }
 
   @override
@@ -237,6 +245,7 @@ class _AnimatedThemeState extends AnimatedWidgetBaseState<AnimatedTheme> {
   @override
   void debugFillProperties(DiagnosticPropertiesBuilder description) {
     super.debugFillProperties(description);
-    description.add(DiagnosticsProperty<ThemeDataTween>('data', _data, showName: false, defaultValue: null));
+    description.add(DiagnosticsProperty<ThemeDataTween>('data', _data,
+        showName: false, defaultValue: null));
   }
 }
